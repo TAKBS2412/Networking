90c28cff20eb3c9ec58ab16dddb52be6823ab4ac
s-engt <s-engt@52S50012454.it.bsd405.org>
2018-12-31 17:06:46 -0800

    Run UDP send in a loop (python).

--- python/UDPSend.py
+++ python/UDPSend.py
@@ -10,4 +10,14 @@ print("Message: " + MESSAGE)
 
 sock = socket.socket(socket.AF_INET, # Internet
     socket.SOCK_DGRAM) # UDP
-sock.sendto(MESSAGE.encode('utf-8'), (UDP_IP, UDP_PORT))
+
+while True:
+    print("Enter message: ", end="")
+    MESSAGE = input()
+    
+    sock.sendto(MESSAGE.encode('utf-8'), (UDP_IP, UDP_PORT))
+    
+    if MESSAGE == "":
+        break
+#     data = s.recv(BUFFER_SIZE)
+#     print("Received data:" + str(data))
